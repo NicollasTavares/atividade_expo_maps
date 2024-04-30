2da1fd6d33823280f103f7b3d143436e96eb5143
Nicollas Tavares <nicollascursos@gmail.com>
2024-04-29 21:44:35 -0300

    subindo ao git

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useRef, useState } from "react";
+import MapView, { Marker } from 'react-native-maps';
+import { View } from 'react-native';
+
+import {
+  requestForegroundPermissionsAsync, // Solicita ao usuário a permissão para utilizar a localização
+  getCurrentPositionAsync, // Quando autorizado nos retorna a posição do usuário
+  watchPositionAsync, // Observa mudança na localização da posição
+  LocationAccuracy,
+
+} from 'expo-location';
+import { styles } from "./src/styles/style";
+
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+
+  const mapRef = useRef(null);
+
+  // Solicita ao usuário a permissão para acessar a localização
+  async function requestLocationPermissions() {
+    const { granted } = await requestForegroundPermissionsAsync();
+    // Caso o resultado da variável 'granted' seja true, a posição atual é retornada para a variável 'currentPosition'
+
+    if (granted) {
+      const currentPosition = await getCurrentPositionAsync();
+      setLocation(currentPosition)
+      // Exibindo no console as informações da posição atual
+      console.log(`LOCALIZAÇÃO ATUAL => `, currentPosition);
+    }
+  }
+
+  useEffect(() => {
+    requestLocationPermissions()
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {
+        // Renderiza o componente caso a variável esteja definida, ou seja, diferente de null. '&&' é um operador lógico utilizado em JavaScript para avaliação condicional.
+        location &&
+        <MapView
+          ref={mapRef}
+          style={styles.map}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude
+            }}
+          />
+        </MapView>
+      }
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
